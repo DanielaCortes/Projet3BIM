f66ebce6da7ae2dd979f3b1ca9c7e543de60855b
Yasmina Adimy <yadimy@localhost.localdomain>
2016-06-18 10:46:21 +0200

    correction et  merge tout va bien

--- Sea.py
+++ Sea.py
@@ -109,8 +109,8 @@ class Sea :
         req1_lateral = []
         compteur_req1 = []
         for i in range (len(self.zones)):
-            mean_sharks_ventral = [[0 for i in range (len(self.zones[i].sharks[j].tab_ventral)] for j in range (len(self.zones[i].sharks[j].tab_ventral[0]))]
-            mean_sharks_lateral = [[0 for i in range (len(self.zones[i].sharks[j].tab_lateral)] for j in range (len(self.zones[i].sharks[j].tab_lateral[0]))]
+            mean_sharks_ventral = [[0 for i in range (len(self.zones[i].sharks[j].tab_ventral))] for j in range (len(self.zones[i].sharks[j].tab_ventral[0]))]
+            mean_sharks_lateral = [[0 for i in range (len(self.zones[i].sharks[j].tab_lateral))] for j in range (len(self.zones[i].sharks[j].tab_lateral[0]))]
             for j in range (len(self.zones.sharks)):
                 mean_sharks_ventral += self.zones[i].sharks[j].tab_ventral
                 mean_sharks_lateral += self.zones[i].sharks[j].tab_lateral
